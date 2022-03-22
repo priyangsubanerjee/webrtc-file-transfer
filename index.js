396077ed42db7d186e24e4d646942533ab4796ba
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-03-22 12:53:07 +0530

    default port 3000

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const path = require("path");
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
-server.listen(process.env.PORT || 4000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
